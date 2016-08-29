9c2155b3620f52b5ab872e29b14c3ae41373e6ce
Paul <paulrobnett@hotmail.com>
2016-08-28 22:59:30 -0500

    baby steps, long way to go still

--- main.py
+++ main.py
@@ -16,6 +16,7 @@
 #
 import webapp2
 import cgi
+import re
 
 # html boilerplate for the top of every page
 page_header = """
@@ -24,12 +25,13 @@ page_header = """
 <head>
     <title>User Signup</title>
     <style type="text/css">
-        body {float:left;background-color:tan;}
-        .error {color: red;}
-        .form {width:200px;background-color:#ccc;padding:7px;}
+        body {background-color:tan;margin-left:300px;}
+        .form {width:500px;background-color:#ccc;padding:7px;}
+        div {}
         label {margin:25px 0px 0px 0px;}
-        div {float:left;}
-        .error {float:left;color:red;display:inline;}
+        input {float;left;}
+        .error {float:right;color:red;display:inline;}
+        .clear {clear:both;}
 
     </style>
 </head>
@@ -38,47 +40,52 @@ page_header = """
         <a href="/">User Signup</a>
     </h3>
 """
-
 signup_form = """
-<form action="/signup" method="POST" class="form">
-    <div>
-        <label>Username:
-            <input type="text" name="username" value="%(username)s" />
-            <span class="error"> %(e_username)s</span>
-        </label>
-    </div>
-    <div>
-        <label>Password:
-            <input type="password" name="password" value="%(password)s" />
-            <span class="error"> %(e_password)s</span>
-        </label>
-    </div>
-    <div>
-        <label>Verify Password:
-            <input type="password" name="verify" value="%(verify)s" />
-            <span class="error"> %(e_verify)s</span>
-        </label>
-    </div>
-    <div>
-        <label>Email(optional):
-            <input type="text" name="email" value="%(email)s" />
-            <span class="error"> %(e_email)s</span>
-        </label>
-    </div>
+<form action="/" method="POST" class="form">
+    <table>
+        <tr>
+            <td><label>Username:</td>
+            <td><input type="text" name="username" value="" /></td>
+            <td><span class="error"> %(e_username)s</span></td>
+            </label>
+        </tr>
+        <tr>
+            <td><label>Password:</td>
+            <td><input type="password" name="password" value="" /></td>
+            <td><span class="error"> %(e_password)s</span</td>
+            </label>
+        </tr>
+        <tr>
+            <td><label>Verify Password:</td>
+            <td><input type="password" name="verify" value="" /></td>
+            <td><span class="error"> %(e_verify)s</span></td>
+            </label>
+        </tr>
+        <tr>
+            <td><label>Email(optional):</td>
+            <td><input type="text" name="email" value="" /></td>
+            <td><span class="error"> %(e_email)s</span></td>
+            </label>
+        </tr>
+    </table>
     <input type="submit" value="Submit Info" /><br />
 </form>
 """
 
-
 # html boilerplate for the bottom of every page
 page_footer = """
-</body>copyright 2016
+</body><br />copyright 2016
 </html>
 """
-
+#not sure where to put these
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+PASS_RE = re.compile(r"^.{3,20}$")
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+#   errors['username_error']  = ""
 
 class Index(webapp2.RequestHandler):
 
+    #  look inside for and acccess input and/or use to post errors
     def write_form(self, e_username="", e_password="", e_verify="", e_email="", username="", password="", verify="", email=""):
         self.response.out.write(page_header + signup_form % {"e_username": e_username,
                                                 "e_password": e_password,
@@ -88,38 +95,50 @@ class Index(webapp2.RequestHandler):
                                                 "password": password,
                                                 "verify": verify,
                                                 "email": email} + page_footer)
+                                                # "name" to value
 
+
+
+    #  print blank form on initial page load
     def get(self):
-        #PUT FORM ON SCREEN
-        #response = page_header + rotate_form_input + page_footer
         self.write_form()
-        #self.response.write('You Must Signup To Use Our Site')
 
-class Output(webapp2.RequestHandler):
+
+    #  not sure where to put this
+    def valid_username(username):
+        # udacity regular expression to validate username input
+        if not USER_RE.match(input_username) in valid_username():
+            errors['e_username'] = "username not valid, letter and numbers only please"
+            return USER_RE.match(errors)
+
+
+    #  pull out the inputs
     def post(self):
-        # look inside the request to figure out what the user typed
+        # look inside the request to figure out what the user typed and store here
         input_username = self.request.get('username')
         input_password = self.request.get('password')
         input_verify = self.request.get('verify')
         input_email = self.request.get('email')
 
-        # validation procedures
-        e_username = "You must create a user name."
+        # my own simple check to see if anything was put in username
         if input_username == '':
-            err_response = signup_form.format(e_username = e_username)
-            self.redirect("/?error=" + err_response)
+            e_username = "You must create a user name"
+            # stay on index, and include error next to form field
+            self.write_form(e_username)
 
 
-        error_pass_unmatch = "Your passwords do not match"
-        if input_password != input_verify:
-            self.redirect("/?error=" + error_pass_unmatch)
 
-        # combine all the pieces to build the content of our response
-        main_content = input_username + input_password + input_verify + input_email
-        response = page_header + main_content + page_footer
-        self.response.out.write(response)
+
+
+        # if all the IF statements pass verification, replace form with welcome and username
+        else:
+            success = "Welcome, " + input_username
+            greeting = page_header + success + page_footer
+            self.response.out.write(greeting)
+
+
+
 
 app = webapp2.WSGIApplication([
     ('/', Index),
-    ('/signup', Output)
 ], debug=True)
